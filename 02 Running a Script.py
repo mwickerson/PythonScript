fccd1863ecab8408f46c4c578b0d71230b374885
Michael Wickerson <51535473+mwickerson@users.noreply.github.com>
2019-12-12 05:03:21 -0600

    Add files via upload

---
+++ 02 Running a Script.py
@@ -0,0 +1,14 @@
+#Comments
+
+#To place some instructions that won't be executed by the program
+#Use the '#' in front of your text.
+#This is called a comment and is extremely useful for both
+#yourself and anyone else that needs to read your code
+
+#Testing & Debugging Code
+
+#To write something in the console below - use print
+#This helps a lot to figure out what your code is doing
+#To print out text, it must be displayed within quotation marks
+
+print "Hello Rhino"
